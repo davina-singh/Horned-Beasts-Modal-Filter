bdfa5c404c1476ed16acda5ac1877e92d7ce2724
DAVINA SINGH <davina_singh@outlook.com>
2024-06-04 13:25:19 +0100

    name-change

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Gallery from "./components/Gallery";
+import data from "./assets/data.json";
+import { useState } from "react";
+import SelectedBeast from "./components/SelectedBeast";
+
+function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [shownBeast, setShownBeast] = useState({});
+  const [horns, setHorns] = useState("");
+
+  function handleShowModal(beast) {
+    setShowModal(!showModal);
+    setShownBeast(beast);
+  }
+  
+  function handleFilter(event) {
+    setHorns( event.target.value);
+  }
+
+  return (
+    <div>
+    <Header />
+        <select onChange={handleFilter}> 
+          <option value="">All</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="100">100</option>
+        </select>
+    <Gallery data={data} handleShowModal={handleShowModal} horns={horns} />   
+    <Footer />
+    {showModal && (
+    <SelectedBeast 
+      shownBeast={shownBeast} 
+      handleShowModal={handleShowModal}
+      /> 
+    )}
+    </div>
+  );
+}
+
+export default App;
